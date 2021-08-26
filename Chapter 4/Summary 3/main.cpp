c186a10b7e9ca16fa1b918b93389bdb6c64c617e
dovolniyzavodchanin@yandex.ru <dovolniyzavodchanin@yandex.ru>
2021-08-26 12:35:23 +0300

    Fixed 3 bad practices

--- Chapter 4/Summary 3/main.cpp
+++ Chapter 4/Summary 3/main.cpp
@@ -7,19 +7,21 @@ Find 3 issues (affecting 4 lines) in the following code.
 #include <cstdint>
 #include <iostream>
 
+constexpr int drivingAge{ 16 };
+
 int main()
 {
     std::cout << "How old are you?\n";
 
-    std::uint8_t age{};
+    int age{};
     std::cin >> age;
 
     std::cout << "Allowed to drive a car in Texas [";
 
-    if (age >= 16)
-        std::cout << "x";
+    if (age >= drivingAge)
+        std::cout << 'x';
     else
-        std::cout << " ";
+        std::cout << ' ';
 
     std::cout << "]\n";
 
