381a1fec3ede8a3ae548a5368823809f81a6e51c
dovolniyzavodchanin@yandex.ru <dovolniyzavodchanin@yandex.ru>
2021-08-26 12:00:42 +0300

    First launch for question # $

---
+++ Chapter 4/Summary 2/main.cpp
@@ -0,0 +1,14 @@
+/*
+Write a short program to simulate a ball being dropped off of a tower. To start, the user should be asked for the height of the tower in meters.
+Assume normal gravity (9.8 m/s2), and that the ball has no initial velocity (the ball is not moving to start).
+Have the program output the height of the ball above the ground after 0, 1, 2, 3, 4, and 5 seconds. The ball should not go underneath the ground (height 0).
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "test";
+
+	return 0;
+}
