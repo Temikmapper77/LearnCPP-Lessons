b99bca8c2b84bf829c4ea956814fbe8cb4ba71ae
dovolniyzavodchanin@yandex.ru <dovolniyzavodchanin@yandex.ru>
2021-08-27 15:24:37 +0300

    Task 2 done

---
+++ Chapter 6/Summary 2/main.cpp
@@ -0,0 +1,23 @@
+/*
+Write a file named constants.h that makes the following program run. 
+If your compiler is C++17 capable, use inline constexpr variables. 
+Otherwise, use normal constexpr variables.
+*/
+
+#include <iostream>
+#include "constants.h"
+
+int main()
+{
+	std::cout << "How many students are in your class? ";
+	int students{};
+	std::cin >> students;
+
+
+	if (students > constants::max_class_size)
+		std::cout << "There are too many students in this class";
+	else
+		std::cout << "This class isn't too large";
+
+	return 0;
+}
